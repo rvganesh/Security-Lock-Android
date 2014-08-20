fb869658037b269fc03498e957d155773c561c3e
Sebastian Manolescu <sebastian.manolescu@gmail.com>
2014-08-20 12:59:26 +0300

    Add Account Data (account name / password / details)

--- app/src/main/java/softwareinclude/ro/securitylockandroid/dialog/ItemAddDialog.java
+++ app/src/main/java/softwareinclude/ro/securitylockandroid/dialog/ItemAddDialog.java
@@ -2,10 +2,14 @@ package softwareinclude.ro.securitylockandroid.dialog;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import softwareinclude.ro.securitylockandroid.R;
 
@@ -17,6 +21,12 @@ public class ItemAddDialog extends Dialog implements View.OnClickListener{
     private Context context;
     private Button cancelAdd;
     private Button doneAdd;
+    private EditText accountItemName;
+    private EditText accountItemPassword;
+    private EditText detailsItemInput;
+    private ImageButton addDetailsItem;
+    private LinearLayout addDetailsLayout;
+
 
     public ItemAddDialog(Context context) {
         super(context);
@@ -29,16 +39,30 @@ public class ItemAddDialog extends Dialog implements View.OnClickListener{
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.add_item_dialog);
 
-        init();
+        initUI();
+        initData();
     }
 
-    public void init(){
+    private void initUI(){
 
         cancelAdd = (Button) findViewById(R.id.cancelAdd);
         cancelAdd.setOnClickListener(this);
         doneAdd = (Button) findViewById(R.id.doneAdd);
         doneAdd.setOnClickListener(this);
 
+        accountItemName = (EditText) findViewById(R.id.accountItemName);
+        accountItemPassword = (EditText) findViewById(R.id.accountItemPassword);
+        detailsItemInput = (EditText) findViewById(R.id.detailsItemInput);
+
+        addDetailsItem = (ImageButton) findViewById(R.id.addDetailsItem);
+        addDetailsItem.setOnClickListener(this);
+
+        addDetailsLayout = (LinearLayout) findViewById(R.id.addDetailsLayout);
+
+    }
+
+    private void initData() {
+        detailsItemInput.setVisibility(View.GONE);
     }
 
 
@@ -53,6 +77,12 @@ public class ItemAddDialog extends Dialog implements View.OnClickListener{
 
             case R.id.doneAdd: {
                 dismiss();
+                break;
+            }
+
+            case R.id.addDetailsItem: {
+                addDetailsLayout.setVisibility(View.GONE);
+                detailsItemInput.setVisibility(View.VISIBLE);
                 break;
             }
 
